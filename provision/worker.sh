43324a858a969ee359e53bafc7365b588493edd6
Azman Kudus <aa.azmankudus@outlook.my>
2024-11-19 02:52:35 +0800

    relocate provision files

--- provision/worker.sh
+++ provision/worker.sh
@@ -3,9 +3,13 @@
 WORKER_IP="$1"
 
 # Join cluster
-bash /vagrant/join.sh
+bash /vagrant/cluster/join.sh
+
+# Update node IP
 REGEX_WORKER_IP="$(echo ${WORKER_IP} | sed 's/\./\\\./g')"
 sed -i "/KUBELET_KUBEADM_ARGS/ s/\"$/ \-\-node\-ip\=${REGEX_WORKER_IP}\"/" /var/lib/kubelet/kubeadm-flags.env
 systemctl restart kubelet
+
+# Update user config
 mkdir $HOME/.kube
-cp -p /vagrant/admin.conf $HOME/.kube/config
+cp -p /vagrant/cluster/admin.conf $HOME/.kube/config
