f7fea72c2dcde1c5fc6916dc2d96c19cb0ffefe2
Azman Kudus <aa.azmankudus@outlook.my>
2025-01-08 02:21:08 +0800

    #14: create ssh config for all nodes

--- provision/hosts.sh
+++ provision/hosts.sh
@@ -4,12 +4,25 @@
 echo '' >> /etc/hosts
 echo '# Kubernetes nodes' >> /etc/hosts
 
+rm -f /root/.ssh/config
 ARGS=("$@")
 for ((i=0; i<${#ARGS[@]}; i++)); do
   IP=${ARGS[i]}
   NAME=${ARGS[++i]:-none}
+
+  # update hosts entries
   sed -i "/${NAME}/d" /etc/hosts
   printf "%-16s %s \n" ${IP} ${NAME} >> /etc/hosts
+
+  # create ssh config
+  cat <<EOF >> /root/.ssh/config
+Host ${NAME}
+  Hostname ${NAME}
+  User root
+  PubKeyAuthentication yes
+  IdentityFile /root/.ssh/id_ed25519
+  StrictHostKeyChecking no
+EOF
 done
 
 # Auto login to root
--- provision/master.sh
+++ provision/master.sh
@@ -33,16 +33,6 @@ mkdir /root/.ssh
 ssh-keygen -t ed25519 -C 'kubernetes' -f /root/.ssh/id_ed25519 -P ''
 cat /root/.ssh/id_ed25519.pub >> /root/.ssh/authorized_keys
 
-cat <<EOF > /root/.ssh/config
-Host master
-  Hostname master
-  User root
-  PubKeyAuthentication yes
-  IdentityFile /root/.ssh/id_ed25519
-  StrictHostKeyChecking no
-  UserKnownHostsFile /dev/null
-EOF
-
 chmod 600 /root/.ssh/*
 chmod 700 /root/.ssh
 cp -rp /root/.ssh /vagrant/
